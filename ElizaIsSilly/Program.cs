03937fddead1196e49b8adf992064e6ddd259247
ahmedsport <ahmedsport@hotmail.com>
2022-08-28 20:59:17 +0100

    first commit

---
+++ ElizaIsSilly/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+
+namespace ElizaIsSilly
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Doctor.Intro());
+
+            var userInput = "";
+
+            while (!userInput.Equals("quit", StringComparison.OrdinalIgnoreCase))
+            {
+                userInput = Console.ReadLine();
+
+                string response = Doctor.Response(userInput);
+                Console.WriteLine(response);
+            }
+           
+        }
+    }
+}
