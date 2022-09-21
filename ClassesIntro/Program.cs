dba888aa18e5b02cabfefe2bd115accd94f01921
ahmedsport <ahmedsport@hotmail.com>
2022-09-21 14:13:56 +0100

    hero & monster

---
+++ ClassesIntro/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+
+namespace ClassesIntro
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Car myCare = new Car();
+            myCare.Accelerate();
+            myCare.Accelerate();
+            myCare.Accelerate();
+            myCare.Accelerate();
+            myCare.Accelerate();
+            myCare.Brake();
+            myCare.Accelerate();
+
+        }
+    }
+
+    class Car
+    {
+        private int speed = 0;
+        public void Accelerate()
+        {
+            speed++;
+            Console.WriteLine($"You're going {speed} kilometers per hour.");
+        }
+
+        public void Brake()
+        {
+            speed--;
+            Console.WriteLine($"You're going {speed} kilometers per hour.");
+        }
+    }
+}
